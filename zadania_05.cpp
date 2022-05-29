6d460ab1a6e9ba90badfee38959d297722968b94
jojaczopulos <100236181+jojaczopulos@users.noreply.github.com>
2022-05-29 22:54:29 +0200

    Add files via upload

--- zadania_05.cpp
+++ zadania_05.cpp
@@ -11,6 +11,8 @@ void remove_raw_string(char &argument);
 int main(int argc, char* argv[]){
 
 zadanie_0::create_raw_string();
+zadanie_0::remove_raw_string(zadanie_0::create_raw_string());
+  //zadanie_0::display(14,zadanie_0::time_converter(14));
 
 }
 
@@ -26,23 +28,31 @@ std::cin >> array;
 
 int lenght = std::strlen(array);
 
-//std::cout << "ilosc znakow w tablicy to: " << lenght << std::endl; // liczymy znaki
+std::cout << "ilosc znakow w tablicy to: " << lenght << std::endl; // liczymy znaki
+std::cout << "rozmiar tablicy array to: " << sizeof(array)<< std::endl;
 
 char* buffer = (char*)malloc(lenght);
 
 for(int i= 0; i < lenght; i++){
 
     buffer[i] = array[i];
+    /*if (array[i] == NULL) {
+      break;
+    }*/
 
 }
 //std::cout <<" skopiowana tablica: " << buffer << std::endl;
-
+std::cout << "rozmiar tablicy buffer to: " << sizeof(buffer)<< std::endl;
 return buffer;
+//free (buffer) ;
+
 }
 
 void remove_raw_string(char &argument){
 
-    argument = 
+   char *clear = &argument;s
+   free (clear);
+
 }
 
 
